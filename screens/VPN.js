cbb7e06fed90049232d1e2d6d0d0e5796906edda
victorbruce <victorbruce82@gmail.com>
2020-03-21 23:51:05 +0000

    Style connected status button

--- screens/VPN.js
+++ screens/VPN.js
@@ -1,19 +1,60 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
+import { Block, Text, Utils, Button } from "expo-ui-kit";
+
+// constants
+import { images } from "../constants/";
+
+const { theme, rgba } = Utils;
+const { SIZES, COLORS } = theme;
 
 const VPN = () => {
   return (
-    <View style={styles.container}>
-      <Text>VPN screen</Text>
-    </View>
+    <Block safe center>
+      <Block flex={false} padding={[30, 0]}>
+        <Text title semibold>
+          VPN
+        </Text>
+      </Block>
+
+      <Block center middle>
+        <Block flex={false} row center middle>
+          <Text subtitle semibold gray>
+            CONNECTED
+          </Text>
+          <Block radius={10} color={COLORS.success} style={styles.status} />
+        </Block>
+
+        <Image source={images.icons.offline} style={styles.image} />
+
+        <Button outlined style={styles.connect}>
+          <Text caption center bold margin={[20, 0]}>
+            CONNECTED
+          </Text>
+        </Button>
+      </Block>
+
+      <Block flex={false} middle center white shadow style={{ width: "100%" }}>
+        <Text>Servers</Text>
+      </Block>
+    </Block>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+  connect: {
+    width: SIZES.width / 2
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginVertical: 20
+  },
+  status: {
+    width: 8,
+    height: 8,
+    marginLeft: 10,
+    backgroundColor: "green"
   }
 });
 
